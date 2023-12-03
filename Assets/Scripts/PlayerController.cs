77712ed8b7a755fa16fc6caff293329fbf24bca1
FabianZarva <53430230+FabianZarva@users.noreply.github.com>
2023-12-03 13:11:10 +0100

    Added the turning mechanic, sliding, cinemachine camera and made the game actually playable for the first time, endlessly!
    So, for this week's update, i have added:
    - turning mechanic( in the intersection, the player can turn left or right using the turn inputs, at the edge of the corner(turning threshold);
    - sliding( which is an animator for the player character which modifies both his mesh and shrinks his character controller so he does not hit the obstacle while sliding under it), can be triggered using the slide input;
    -added cinemachine virtual camera, aspect ratio 9:16(average phone screen ratio), which tracks the player and its movements;
    - the game can now be played endlessly, with also obstacle spawns(which now are not often but will modify that in the future).

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,107 +1,187 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Events;
 
-[RequireComponent(typeof(CharacterController), typeof(PlayerInput))]
-public class PlayerController : MonoBehaviour
+namespace EndlessRunner.Player
 {
-    [SerializeField] private float initialPlayerSpeed = 4f;
-    [SerializeField] private float maximumPlayerSpeed = 30f;
-    [SerializeField] private float playerSpeedIncreaseRate = 0.1f;
-    [SerializeField] private float jumpHeight = 1.0f;
-    [SerializeField] private float initialGravityValue = -9.81f;
-    [SerializeField] private LayerMask groundLayer;
 
-    private float playerSpeed;
-    private float gravity;
-    private Vector3 movementDirection = Vector3.forward;
-    private Vector3 playerVelocity;
 
-    private PlayerInput playerInput;
-    private InputAction turnAction;
-    private InputAction jumpAction;
-    private InputAction slideAction;
 
-    private CharacterController controller;
-
-    private void Awake()
+    [RequireComponent(typeof(CharacterController), typeof(PlayerInput))]
+    public class PlayerController : MonoBehaviour
     {
-        playerInput = GetComponent<PlayerInput>();
-        controller = GetComponent<CharacterController>();
-        turnAction = playerInput.actions["Turn"];
-        jumpAction = playerInput.actions["Jump"];
-        slideAction = playerInput.actions["Slide"];
+        [SerializeField] private float initialPlayerSpeed = 4f;
+        [SerializeField] private float maximumPlayerSpeed = 30f;
+        [SerializeField] private float playerSpeedIncreaseRate = 0.1f;
+        [SerializeField] private float jumpHeight = 1.0f;
+        [SerializeField] private float initialGravityValue = -9.81f;
+        [SerializeField] private LayerMask groundLayer;
+        [SerializeField] private LayerMask turnLayer;
+        [SerializeField] private Animator animator;
+        [SerializeField] private AnimationClip slideAnimationClip;
 
+        private float playerSpeed;
+        private float gravity;
+        private Vector3 movementDirection = Vector3.forward;
+        private Vector3 playerVelocity;
+
+        private PlayerInput playerInput;
+        private InputAction turnAction;
+        private InputAction jumpAction;
+        private InputAction slideAction;
+
+        private CharacterController controller;
         
-    }
+        private int slidingAnimationId;
 
-    private void OnEnable()
-    {
-        turnAction.performed += PlayerTurn;
-        slideAction.performed += PlayerSlide;
-        jumpAction.performed += PlayerJump;
-    }
+        private bool sliding = false;
 
-    private void OnDisable()
-    {
-        turnAction.performed -= PlayerTurn;
-        slideAction.performed -= PlayerSlide;
-        jumpAction.performed -= PlayerJump;
-    }
+        [SerializeField] private UnityEvent<Vector3> turnEvent;
 
-    private void Start()
-    {
-        playerSpeed = initialPlayerSpeed;
-        gravity = initialGravityValue;
-    }
-
-    private void PlayerTurn(InputAction.CallbackContext context)
-    {
-
-    }
-    private void PlayerSlide(InputAction.CallbackContext context)
-    {
-
-    }
-    private void PlayerJump(InputAction.CallbackContext context)
-    {
-        if (IsGrounded())
+        private void Awake()
         {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * gravity * -3f);
+            playerInput = GetComponent<PlayerInput>();
+            controller = GetComponent<CharacterController>();
+            
+            slidingAnimationId = Animator.StringToHash("Sliding");
+
+            turnAction = playerInput.actions["Turn"];
+            jumpAction = playerInput.actions["Jump"];
+            slideAction = playerInput.actions["Slide"];
+
+
+        }
+
+        private void OnEnable()
+        {
+            turnAction.performed += PlayerTurn;
+            slideAction.performed += PlayerSlide;
+            jumpAction.performed += PlayerJump;
+        }
+
+        private void OnDisable()
+        {
+            turnAction.performed -= PlayerTurn;
+            slideAction.performed -= PlayerSlide;
+            jumpAction.performed -= PlayerJump;
+        }
+
+        private void Start()
+        {
+            playerSpeed = initialPlayerSpeed;
+            gravity = initialGravityValue;
+        }
+
+        private void PlayerTurn(InputAction.CallbackContext context)
+        {
+            Vector3? turnPosition = CheckTurn(context.ReadValue<float>());
+            if (!turnPosition.HasValue)
+            {
+                return;
+            }
+           Vector3 targetDirection =  Quaternion.AngleAxis(90 * context.ReadValue<float>(), Vector3.up) * 
+                movementDirection;
+            turnEvent.Invoke(targetDirection);
+            Turn(context.ReadValue<float>(), turnPosition.Value);
+        }
+
+        private Vector3? CheckTurn(float turnValue)
+        {
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, 0.1f, turnLayer);
+            if (hitColliders.Length != 0)
+            {
+                Tile tile = hitColliders[0].transform.parent.GetComponent<Tile>();
+                TileType type = tile.type;
+                if ((type == TileType.LEFT && turnValue == -1) ||
+                    (type == TileType.RIGHT && turnValue == 1) ||
+                    (type == TileType.SIDEWAYS))
+                {
+                    return tile.pivot.position;
+                }
+            }
+            return null;
+        
+        }
+
+        private void Turn(float turnValue, Vector3 turnPosition)
+        {
+            Vector3 tempPlayerPosition = new Vector3(turnPosition.x, transform.position.y, turnPosition.z);
+            controller.enabled = false;
+            transform.position = tempPlayerPosition;
+            controller.enabled = true;
+
+            Quaternion targetRotation = transform.rotation * Quaternion.Euler(0, 90 * turnValue, 0);
+            transform.rotation = targetRotation;
+            movementDirection = transform.forward.normalized;
+        }
+        private void PlayerSlide(InputAction.CallbackContext context)
+        {
+            if(!sliding && IsGrounded())
+            {
+                StartCoroutine(Slide());
+            }
+        }
+
+        private IEnumerator Slide()
+        {
+            sliding = true;
+
+            //Shrink collider so player can slide under obstacle
+            Vector3 originalControllerCenter = controller.center;
+            Vector3 newControllerCenter = originalControllerCenter;
+            controller.height /= 2;
+            newControllerCenter.y -= controller.height / 2;
+            controller.center = newControllerCenter;
+            // Sliding animation played            
+            animator.Play(slidingAnimationId);
+            yield return new WaitForSeconds(slideAnimationClip. length);
+            // Character controller height goes back to normal after sliding
+            controller.height *= 2;
+            controller.center = originalControllerCenter;
+            sliding = false;
+        }
+        private void PlayerJump(InputAction.CallbackContext context)
+        {
+            if (IsGrounded())
+            {
+                playerVelocity.y += Mathf.Sqrt(jumpHeight * gravity * -3f);
+                controller.Move(playerVelocity * Time.deltaTime);
+            }
+        }
+
+        private void Update()
+        {
+            controller.Move(transform.forward * playerSpeed * Time.deltaTime);
+
+            if (IsGrounded() && playerVelocity.y < 0)
+            {
+                playerVelocity.y = 0f;
+            }
+
+            playerVelocity.y += gravity * Time.deltaTime;
             controller.Move(playerVelocity * Time.deltaTime);
         }
-    }
 
-    private void Update ()
-    {
-        controller.Move(transform.forward * playerSpeed * Time.deltaTime);
-
-        if(IsGrounded() && playerVelocity.y < 0)
+        private bool IsGrounded(float length = 0.2f)
         {
-            playerVelocity.y = 0f;
+            Vector3 raycastOriginFirst = transform.position;
+            raycastOriginFirst.y -= controller.height / 2f;
+            raycastOriginFirst.y += 0.1f;
+
+            Vector3 raycastOriginSecond = raycastOriginFirst;
+            raycastOriginFirst -= transform.forward * 0.2f;
+            raycastOriginSecond += transform.forward * 0.2f;
+
+            //Debug.DrawLine(raycastOriginFirst, Vector3.down, Color.green, 2f);
+            //Debug.DrawLine(raycastOriginFirst, Vector3.down, Color.red, 2f);
+
+            if (Physics.Raycast(raycastOriginFirst, Vector3.down, out RaycastHit hit, length, groundLayer) || Physics.Raycast(raycastOriginFirst, Vector3.down, out RaycastHit hit2, length, groundLayer))
+            {
+                return true;
+            }
+            return false;
         }
 
-        playerVelocity.y += gravity * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
-    }
-
-    private bool IsGrounded(float length = 0.2f)
-    {
-        Vector3 raycastOriginFirst = transform.position;
-        raycastOriginFirst.y -= controller.height / 2f;
-        raycastOriginFirst.y += 0.1f;
-
-        Vector3 raycastOriginSecond = raycastOriginFirst;
-        raycastOriginFirst -= transform.forward * 0.2f;
-        raycastOriginSecond += transform.forward * 0.2f;
-
-        Debug.DrawLine(raycastOriginFirst, Vector3.down, Color.green, 2f);
-        Debug.DrawLine(raycastOriginFirst, Vector3.down, Color.red, 2f);
-
-        if (Physics.Raycast(raycastOriginFirst, Vector3.down, out RaycastHit hit, length, groundLayer) || Physics.Raycast(raycastOriginFirst, Vector3.down, out RaycastHit hit2, length, groundLayer))
-        {
-            return true;
-        }
-        return false;
     }
 }
