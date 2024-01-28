be97ff330aa466d3c39b7b7eb17baf539640684f
FabianZarva <53430230+FabianZarva@users.noreply.github.com>
2024-01-28 13:27:01 +0100

    final touches.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,17 +11,27 @@ public class GameManager : MonoBehaviour
     private IEnumerator Start()
     { 
         bool connected = false;
+
+        // Starts a guest session using the "LootLocker" SDK
         LootLockerSDKManager.StartGuestSession((response) =>
         {
+            // If the guest session startup is not succesful, then display an error message in the log.
             if (!response.success)
             {
                 Debug.Log("Error on session startup");
                 return;
             }
+            // If the guest session startup is  succesful, then display a successful message in the log.
             Debug.Log("Successful Startup of session");
+
+            // the player is now connected to the LootLocker Server and leaderboard for score tracking.
             connected = true;
         });    
+
+        // waits until the player is connected. 
          yield return new WaitUntil(() => connected);
+
+         // then invokes the unity event of the connected player.
         playerConnected.Invoke();   
             
     }
